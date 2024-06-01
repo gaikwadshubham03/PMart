8aad389058a3a59e3b25fae697596c423640df3e
Shubham Subhash Gaikwad <ShubhamSubhash.Gaikwad@philips.com>
2024-06-01 14:02:11 -0400

    initial commit

---
+++ PMart/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PMart.Models;
+
+
+namespace PMart.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CartController : ControllerBase
+	{
+		private static List<Item> cartItems = new List<Item>();
+
+		[HttpPost]
+		public IActionResult AddItem([FromBody] Item item)
+		{
+			if (item == null || item.Quantity <= 0)
+			{
+				return BadRequest("Invalid item data");
+			}
+			cartItems.Add(item);
+			return Ok(item);
+		}
+
+
+		[HttpGet]
+		public IActionResult GetItems()
+		{
+			return Ok(cartItems);
+		}
+	}
+}
---
+++ PMart/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PMart.Models
+{
+	public class Item
+	{
+		[Required]
+		public string Name { get; set; }
+
+		[Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
+		public double Price { get; set; }
+
+		[Range(1, int.MaxValue, ErrorMessage = "Item should have atleast 1 quantity")]
+		public int Quantity { get; set; }
+
+		public Item(string name, double price, int quantity)
+		{
+			Name = name;
+			Price = price;
+			Quantity = quantity;
+		}
+
+
+	}
+}
