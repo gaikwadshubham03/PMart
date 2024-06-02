44433f8f3146219afbddad0bd30eec534431d221
Shubham Subhash Gaikwad <ShubhamSubhash.Gaikwad@philips.com>
2024-06-01 22:19:09 -0400

    remove comments and removing shm and wal file

--- PMart/Data/ApplicationDbContext.cs
+++ PMart/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ namespace PMart.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
-		// it is like a abstraction layer which interacts with database to perform any operations
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
 		public DbSet<Item> Items { get; set; }
