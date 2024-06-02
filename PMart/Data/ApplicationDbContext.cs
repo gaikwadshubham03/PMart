45d9b15d30a9aedd3223f3f385089fbaf201733a
Shubham Subhash Gaikwad <ShubhamSubhash.Gaikwad@philips.com>
2024-06-01 20:23:52 -0400

    adding database connection

---
+++ PMart/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMart.Models;
+
+namespace PMart.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		// it is like a abstraction layer which interacts with database to perform any operations
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<Item> Items { get; set; }
+
+	}
+}
--- PMart/Program.cs
+++ PMart/Program.cs
@@ -1,8 +1,17 @@
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using PMart.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+Debug.WriteLine("Connection String :" + connectionString);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlite(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
