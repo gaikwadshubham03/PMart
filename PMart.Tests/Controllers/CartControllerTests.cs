fb5389306842b9d1de201dc88eeb5d210c52c8e9
Shubham Subhash Gaikwad <ShubhamSubhash.Gaikwad@philips.com>
2024-06-03 01:44:27 -0400

    added readme file, updated launchSettings.json

--- PMart.Tests/Controllers/CartControllerTests.cs
+++ PMart.Tests/Controllers/CartControllerTests.cs
@@ -13,163 +13,172 @@ using PMart.Models;
 
 namespace PMart.Tests.Controllers
 {
-    [TestClass]
-    public class CartControllerTests
-    {
-        private DbContextOptions<ApplicationDbContext>? _options;
-        private ApplicationDbContext? _context;
-        private Mock<ILogger<CartController>>? _loggerMock;
+	[TestClass]
+	public class CartControllerTests
+	{
+		private DbContextOptions<ApplicationDbContext>? _options;
+		private ApplicationDbContext? _context;
+		private Mock<ILogger<CartController>>? _loggerMock;
 
-        [TestInitialize]
-        public void Setup()
-        {
-            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
-            _context = new ApplicationDbContext(_options);
-            _loggerMock = new Mock<ILogger<CartController>>();
-        }
+		[TestInitialize]
+		public void Setup()
+		{
+			_options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "TestDatabase")
+				.Options;
+			_context = new ApplicationDbContext(_options);
+			_loggerMock = new Mock<ILogger<CartController>>();
+		}
 
-        [TestMethod]
-        public void CartBothNull()
-        {
-            Assert.ThrowsException<ArgumentNullException>(() => new CartController(null!, null!));
-        }
+		[TestMethod]
+		public void CartBothNull()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new CartController(null!, null!));
+		}
 
-        [TestMethod]
-        public void CartNullContext()
-        {
-            if (_loggerMock != null && _context == null)
-            {
-                Assert.ThrowsException<ArgumentNullException>(
-                    () => new CartController(null!, _loggerMock.Object)
-                );
-            }
-        }
+		[TestMethod]
+		public void CartNullContext()
+		{
+			if (_loggerMock != null && _context == null)
+			{
+				Assert.ThrowsException<ArgumentNullException>(
+					() => new CartController(null!, _loggerMock.Object)
+				);
+			}
+		}
 
-        [TestMethod]
-        public void CartNullLogger()
-        {
-            if (_loggerMock == null && _context != null)
-            {
-                Assert.ThrowsException<ArgumentNullException>(
-                    () => new CartController(_context, null!)
-                );
-            }
-        }
+		[TestMethod]
+		public void CartNullLogger()
+		{
+			if (_loggerMock == null && _context != null)
+			{
+				Assert.ThrowsException<ArgumentNullException>(
+					() => new CartController(_context, null!)
+				);
+			}
+		}
 
-        [TestMethod]
-        public void CartInitizationValid()
-        {
-            if (_context != null && _loggerMock != null)
-            {
-                var controller = new CartController(_context, _loggerMock.Object);
-                Assert.IsNotNull(controller);
-            }
-        }
+		[TestMethod]
+		public void CartInitizationValid()
+		{
+			if (_context != null && _loggerMock != null)
+			{
+				var controller = new CartController(_context, _loggerMock.Object);
+				Assert.IsNotNull(controller);
+			}
+		}
 
-        [TestMethod]
-        public async Task CartAddItemValid()
-        {
-            var context = new ApplicationDbContext(_options!);
-            context.SaveChanges();
+		[TestMethod]
+		public async Task CartAddItemValid()
+		{
+			var context = new ApplicationDbContext(_options!);
+			context.SaveChanges();
 
-            var controller = new CartController(context!, _loggerMock!.Object);
-            var addItem = new ItemDTO("Item2", 5.99, 10);
+			var controller = new CartController(context!, _loggerMock!.Object);
+			var addItem = new ItemDTO("Item2", 5.99, 10);
 
-            var addedItem = await controller.AddItem(addItem);
+			var addedItem = await controller.AddItem(addItem);
 
-            var result = addedItem as OkObjectResult;
-            Assert.IsNotNull(result);
-            var item = result.Value as ItemDTO;
-            Assert.IsNotNull(item);
-            Assert.AreEqual(item.Name, addItem.Name);
-            Assert.AreEqual(item.Price, addItem.Price);
-            Assert.AreEqual(item.Quantity, addItem.Quantity);
-        }
+			var result = addedItem as OkObjectResult;
+			Assert.IsNotNull(result);
+			var item = result.Value as Item;
+			Assert.IsNotNull(item);
+			Assert.AreEqual(item.Name, addItem.Name);
+			Assert.AreEqual(item.Price, addItem.Price);
+			Assert.AreEqual(item.Quantity, addItem.Quantity);
+		}
 
-        [TestMethod]
-        public async Task CartAddInvalidItem()
-        {
-            var context = new ApplicationDbContext(_options!);
-            context.SaveChanges();
+		[TestMethod]
+		public async Task CartAddInvalidItem()
+		{
+			var context = new ApplicationDbContext(_options!);
+			context.SaveChanges();
 
-            var controller = new CartController(context!, _loggerMock!.Object);
+			var controller = new CartController(context!, _loggerMock!.Object);
 
-            var error = await controller.AddItem(null!);
-            var result = error as BadRequestObjectResult;
-            Assert.IsNotNull(result);
-            Assert.AreEqual(result.Value, "Item is null");
-        }
+			var error = await controller.AddItem(null!);
+			var result = error as BadRequestObjectResult;
+			Assert.IsNotNull(result);
+			Assert.AreEqual(result.Value, "Item is null");
+		}
 
-        [TestMethod]
-        public async Task GetEmptyCartItem()
-        {
-            var context = new ApplicationDbContext(_options!);
-            context.SaveChanges();
+		[TestMethod]
+		public async Task GetEmptyCartItem()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "TestDatabase1")
+				.Options;
+			var context = new ApplicationDbContext(options!);
+			context.SaveChanges();
 
-            var controller = new CartController(context!, _loggerMock!.Object);
+			var controller = new CartController(context!, _loggerMock!.Object);
 
-            var list = await controller.GetItems();
+			var list = await controller.GetItems();
 
-            var listItems = (list as OkObjectResult);
+			var listItems = (list as OkObjectResult);
 
-            Assert.IsNotNull(listItems);
-            var items = (listItems.Value) as List<Item>;
-            Assert.IsNotNull(items);
-            Assert.AreEqual(items.Count, 0);
-        }
+			Assert.IsNotNull(listItems);
+			var items = (listItems.Value) as List<Item>;
+			Assert.IsNotNull(items);
+			Assert.AreEqual(items.Count, 0);
+		}
 
-        [TestMethod]
-        public async Task GetSingleCartItem()
-        {
-            var context = new ApplicationDbContext(_options!);
-            context.SaveChanges();
+		[TestMethod]
+		public async Task GetSingleCartItem()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "TestDatabase2")
+				.Options;
+			var context = new ApplicationDbContext(options!);
+			context.SaveChanges();
 
-            var controller = new CartController(context!, _loggerMock!.Object);
-            var addItem = new ItemDTO("Item1", 5.99, 10);
+			var controller = new CartController(context!, _loggerMock!.Object);
+			var addItem = new ItemDTO("Item1", 5.99, 10);
 
-            var addedItem = await controller.AddItem(addItem);
+			var addedItem = await controller.AddItem(addItem);
 
-            var list = await controller.GetItems();
+			var list = await controller.GetItems();
 
-            var listItems = (list as OkObjectResult);
+			var listItems = (list as OkObjectResult);
 
-            Assert.IsNotNull(listItems);
-            var items = (listItems.Value) as List<Item>;
-            Assert.IsNotNull(items);
-            var item = items[0];
-            Assert.AreEqual(item.Name, addItem.Name);
-            Assert.AreEqual(item.Quantity, addItem.Quantity);
-            Assert.AreEqual(item.Price, addItem.Price);
-        }
+			Assert.IsNotNull(listItems);
+			var items = (listItems.Value) as List<Item>;
+			Assert.IsNotNull(items);
+			var item = items[0];
+			Assert.AreEqual(item.Name, addItem.Name);
+			Assert.AreEqual(item.Quantity, addItem.Quantity);
+			Assert.AreEqual(item.Price, addItem.Price);
+		}
 
-        [TestMethod]
-        public async Task GetMultipleCartItem()
-        {
-            var context = new ApplicationDbContext(_options!);
-            context.SaveChanges();
+		[TestMethod]
+		public async Task GetMultipleCartItem()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase(databaseName: "TestDatabase3")
+				.Options;
+			var context = new ApplicationDbContext(options!);
+			context.SaveChanges();
 
-            var controller = new CartController(context!, _loggerMock!.Object);
-            var item1 = new ItemDTO("Item1", 5.99, 10);
-            var item2 = new ItemDTO("Item2", 6, 10);
-            var item3 = new ItemDTO("Item 3", 7, 10);
+			var controller = new CartController(context!, _loggerMock!.Object);
+			var item1 = new ItemDTO("Item1", 5.99, 10);
+			var item2 = new ItemDTO("Item2", 6, 10);
+			var item3 = new ItemDTO("Item 3", 7, 10);
 
-            var result1 = await controller.AddItem(item1);
-            var result2 = await controller.AddItem(item2);
-            var result3 = await controller.AddItem(item3);
+			var result1 = await controller.AddItem(item1);
+			var result2 = await controller.AddItem(item2);
+			var result3 = await controller.AddItem(item3);
 
-            var list = await controller.GetItems();
+			var list = await controller.GetItems();
 
-            var listItems = (list as OkObjectResult);
+			var listItems = (list as OkObjectResult);
 
-            Assert.IsNotNull(listItems);
-            var items = (listItems.Value) as List<Item>;
-            Assert.IsNotNull(items);
-            var item = items[2];
-            Assert.AreEqual(item.Name, item3.Name);
-            Assert.AreEqual(item.Quantity, item3.Quantity);
-            Assert.AreEqual(item.Price, item3.Price);
-        }
-    }
+			Assert.IsNotNull(listItems);
+			var items = (listItems.Value) as List<Item>;
+			Assert.IsNotNull(items);
+			var item = items[2];
+			Assert.AreEqual(item.Name, item3.Name);
+			Assert.AreEqual(item.Quantity, item3.Quantity);
+			Assert.AreEqual(item.Price, item3.Price);
+		}
+	}
 }
--- README.md
+++ README.md
@@ -7,8 +7,11 @@ PMart is a simple shopping cart service developed using ASP.NET Core. This proje
 - [Getting Started](#getting-started)
 - [Prerequisites](#prerequisites)
 - [Installation](#installation)
+- [Run the application](#run-the-application)
 - [API Endpoints](#api-endpoints)
 - [Running the Tests](#running-the-tests)
+- [Prometheus Metrics](#prometheus-metrics)
+
 
 ## Getting Started
 
@@ -30,6 +33,15 @@ Follow these instructions to get a copy of the project up and running on your lo
     cd PMart
     ```
 3. Open Solution file (.sln) in Visual Studio 
+4. Open Package Manager Console and execute following command
+    ```sh
+    PM > Update-Database
+    ```
+
+## Run the application
+
+1. Click on the "PMart" to view the Swagger UI and interact with the API endpoints.
+![alt text](asset/image.png)
 
 
 ## API Endpoints
@@ -58,5 +70,18 @@ Here are the available API endpoints for the shopping cart service:
 - **Method:** `GET`
 - **Response:**
     - `200 OK`: Returns a list of items.
+
+## Running the Tests
+
+To run the unit tests and integration tests:
+
+1. Open `View > Test Explorer` and execute all the tests
+
+![Test Folder](asset/test.png)
+
+## Prometheus Metrics
+The application exposes Prometheus metrics for monitoring purposes. 
+
+To view the metrics locally, run the application and navigate to `localhost/metrics` while interacting with APIs to view the counters increment
 
 
